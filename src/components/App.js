7fd46f50395bf29cc161cb3f775da1ebbde4608c
Matt Miller <matt.f.miller@live.com>
2018-07-20 11:08:23 -0700

    Add routes for amp and pedal header links to app component.

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,12 @@ import PedalCollection from './PedalCollection';
 import GuitarSpecs from './GuitarSpecs';
 import NewGuitarForm from './NewGuitarForm';
 import GuitarWishlist from './GuitarWishlist';
+import AmpSpecs from './AmpSpecs';
+import NewAmpForm from './NewAmpForm';
+import AmpWishlist from './AmpWishlist';
+import PedalSpecs from './PedalSpecs';
+import NewPedalForm from './NewPedalForm';
+import PedalWishlist from './PedalWishlist';
 
 class App extends Component {
   constructor(props) {
@@ -23,16 +29,27 @@ class App extends Component {
 
           <Route exact path='/guitars/new' render={() => <NewGuitarForm/>}/>
 
-        <Route exact path='/guitars/wishlist' render={() => <GuitarWishlist/>}/>
+          <Route exact path='/guitars/wishlist' render={() => <GuitarWishlist/>}/>
 
           <Route exact path='/guitars' render={() => <GuitarCollection/>}/>
 
+          <Route exact path='/amps/new' render={() => <NewAmpForm/>}/>
+
+          <Route exact path='/amps/wishlist' render={() => <AmpWishlist/>}/>
+
           <Route exact path='/amps' render={() => <AmpCollection/>}/>
+
+          <Route exact path='/pedals/new' render={() => <NewPedalForm/>}/>
+
+          <Route exact path='/pedals/wishlist' render={() => <PedalWishlist/>}/>
 
           <Route exact path='/pedals' render={() => <PedalCollection/>}/>
 
-          <Route exact path = '/guitars/:guitarId' render={(props)=><GuitarSpecs path={props} selectedGuitarId={props.match.params.guitarId} />} />
+          <Route exact path = '/guitars/:guitarId' render={(props)=><GuitarSpecs path={props} selectedGuitarId={props.match.params.guitarId}/>}/>
 
+          <Route exact path = '/amps/:ampId' render={(props)=><AmpSpecs path={props} selectedAmpId={props.match.params.ampId}/>}/>
+
+          <Route exact path = '/pedals/:pedalId' render={(props)=><PedalSpecs path={props} selectedPedalId={props.match.params.pedalId}/>}/>
 
         </Switch>
       </div>

5cdb5c21e6f84255198cf295bbade425fec8509e
Matt Miller <matt.f.miller@live.com>
2018-07-19 22:05:25 -0700

    Connect guitar specs to store to display details for specific guitar by id.

--- src/components/GuitarSpecs.js
+++ src/components/GuitarSpecs.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import PropTypes from "prop-types";
 
 function GuitarSpecs(props) {
+  let selectedGuitar;
+  {props.guitarsInCollection.forEach(function(guitar) {
+    if (guitar.id === parseInt(props.selectedGuitarId)) {
+      selectedGuitar = guitar;
+    }
+  });}
+
   return(
     <div>
-      hi
+      <img src={selectedGuitar.imageUrl}/>
     </div>
   );
 }
 
-export default GuitarSpecs;
+const mapStateToProps = state => {
+  return  {
+    guitarsInCollection: state.guitarsInCollection
+  }
+}
+
+export default connect(mapStateToProps)(GuitarSpecs);

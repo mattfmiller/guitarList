e9084dcb557d8722b2235915ff43ceb3e1b2160e
Matt Miller <matt.f.miller@live.com>
2018-07-20 14:05:36 -0700

    Add logic to display guitars in wishlist.

--- src/components/GuitarWishlist.js
+++ src/components/GuitarWishlist.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import PropTypes from "prop-types";
+import Guitar from './Guitar';
+import './styles/List.css';
 
 function GuitarWishlist(props) {
   return(
-    <div>
-      Guitar Wishlist
+    <div className="container">
+      {props.guitarsInWishlist.map((guitar) =>
+         <Guitar imageUrl = {guitar.imageUrl}
+          manufacturer = {guitar.manufacturer}
+          model = {guitar.model}
+          id = {guitar.id}
+          key = {guitar.id} />
+      )}
     </div>
   );
 }
 
-export default GuitarWishlist;
+const mapStateToProps = state => {
+  return  {
+    guitarsInWishlist: state.guitarsInWishlist
+  }
+}
+
+export default connect(mapStateToProps)(GuitarWishlist);
--- src/constants/InitialState.js
+++ src/constants/InitialState.js
@@ -1,5 +1,4 @@
 export const initialState = {
-
   guitarsInCollection: [
     {
         bodyWood: "Mahogany",
@@ -444,6 +443,52 @@ export const initialState = {
       wishlist: false,
       paid: 125,
       sold: 0
+    }
+  ],
+  guitarsInWishlist: [
+    {
+        bodyWood: "Mahogany",
+        finish: "Lacquer",
+        color: "Ebony",
+        binding: "5-Ply",
+        neckWood: "Mahogany",
+        neckType: "Set",
+        neckProfile: "1960's Slim Taper",
+        fretboardWood: "Ebony",
+        fretboardRadius: 12,
+        frets: 22,
+        fretMaterial: "Gibson Style",
+        inlays: "Mother of Pearl Block",
+        nutMaterial: "Bone",
+        nutWidth: 1.6875,
+        scaleLength: 24.75,
+        neckPickup: "Humbucker",
+        middlePickup: "N/A",
+        bridgePickup: "Humbucker",
+        volumePots: "500K CTS",
+        tonePots: "500K CTS",
+        capacitor: "Sprangue .022 uf",
+        tuners: "Grover Rotomatic",
+        tunerButtons: "Nickel Keystone",
+        bridge: "Roller",
+        tailpiece: "Bigsby B7",
+        guitarSwitch: "3-way",
+        knobs: "Bellhat",
+        pickguard: "5-Ply Black",
+        controls: "Volume, Tone",
+        guitarCase: "Gibson Hardcase",
+        id: 1,
+        manufacturer: "Gibson",
+        model: "Les Paul Custom",
+        country: "USA",
+        serialNumber: "xxx",
+        year: 1999,
+        weight: 10.8,
+        imageUrl: "https://www.mansons.co.uk/userfiles/shop/products/main/main_6278.jpg",
+        current: true,
+        wishlist: false,
+        paid: 1500,
+        sold: 0
     }
   ]
 }

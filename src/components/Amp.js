ecccf5ced58a0aecdb0e0deeb212cb571a207ea4
Matt Miller <matt.f.miller@live.com>
2018-07-20 12:19:13 -0700

    Add content to amp and pedal components to display image and name.

--- src/components/Amp.js
+++ src/components/Amp.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import {Link} from 'react-router-dom';
+import './styles/Instrument.css';
 
 function Amp(props) {
+  let linkPath = '/amps/' + props.id;
+  console.log(linkPath);
   return(
-    <div>
-    </div>
+    <Link style={{textDecoration: 'none', color: 'black'}} to={linkPath}><div className='instrument'>
+      <img src={props.imageUrl}/>
+      <h2>{props.manufacturer} - {props.model}</h2>
+    </div></Link>
   );
 }
+
+Amp.propTypes = {
+  imageUrl:PropTypes.string,
+  manufacturer:PropTypes.string,
+  model:PropTypes.string,
+  id:PropTypes.number,
+};
 
 export default Amp;
--- src/components/Pedal.js
+++ src/components/Pedal.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import {Link} from 'react-router-dom';
+import './styles/Instrument.css';
 
 function Pedal(props) {
+  let linkPath = '/pedals/' + props.id;
+  console.log(linkPath);
   return(
-    <div>
-    </div>
+    <Link style={{textDecoration: 'none', color: 'black'}} to={linkPath}><div className='instrument'>
+      <img src={props.imageUrl}/>
+      <h2>{props.manufacturer} - {props.model}</h2>
+    </div></Link>
   );
 }
+
+Pedal.propTypes = {
+  imageUrl:PropTypes.string,
+  manufacturer:PropTypes.string,
+  model:PropTypes.string,
+  id:PropTypes.number,
+};
 
 export default Pedal;
